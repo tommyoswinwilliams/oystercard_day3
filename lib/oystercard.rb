fc011d236176f8804a3780367f5563ab959c6ed5
parsa <parsamesg@gmail.com>
2021-09-27 15:38:41 +0100

    refactor oystercard methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,7 +6,13 @@ class Oystercard
   end
 
   def top_up(amount)
-    fail "Please provide a valid amount" unless amount.is_a?(Integer) || amount.is_a?(Float)
+    fail "Please provide a valid amount" unless valid_amount?(amount)
     @balance += amount
+  end
+
+  private
+
+  def valid_amount?(amount)
+    (amount.is_a?(Integer) || amount.is_a?(Float)) && (amount.positive?)
   end
 end
