430cb004d056b1f929a44eeaac1b41824b5a90a7
parsa <parsamesg@gmail.com>
2021-09-27 15:30:52 +0100

    pass unit test to top up balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 class Oystercard
   attr_reader :balance
-  
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    fail "Please provide a valid amount" unless amount.is_a?(Integer) || amount.is_a?(Float)
+    @balance += amount
   end
 end
