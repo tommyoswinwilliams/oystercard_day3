148696e8a6cef6f4e4bcbded2a327a1bd8fa4f06
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-27 14:55:17 +0100

    Add issue comments

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
 describe Oystercard do
   
 end
+
+# NameError without Oystercard class
+# Error occurred at ./spec/oystercard_spec.rb
+# Error occurred at line 1
+# NameError: Raised when a given name is invalid or undefined.
+# Error can be solved by creating a class called Oystercard
