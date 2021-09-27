af7a481e875e0298938fbba13798a87495243e62
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-27 16:28:23 +0100

    Pass unit test for touch_in and in_journey?

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -9,17 +9,26 @@ class Oystercard
       exceed_limit: "Sorry, you cannot exceed the balance limit of £#{BALANCE_LIMIT}",
       insufficient_balance: "Sorry, your balance is not enough to cover the fare" 
     }
+    @in_journey = false
   end
-
+  
   def top_up(amount)
     fail @error_messages[:valid_amount] unless valid_amount?(amount)
     fail @error_messages[:exceed_limit] if exceed_limit?(amount)
     @balance += amount
   end
-
+  
   def deduct(fare)
     fail @error_messages[:insufficient_balance] if fare_exceeds?(fare)
     @balance -= fare
+  end
+  
+  def touch_in
+    @in_journey = true
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   private
@@ -35,4 +44,5 @@ class Oystercard
   def fare_exceeds?(fare)
     @balance < fare
   end
+
 end
