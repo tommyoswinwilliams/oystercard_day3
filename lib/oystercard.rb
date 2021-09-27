05bebd2c6a197e9a705e4e0743531fbc1fd5a7af
parsa <parsamesg@gmail.com>
2021-09-27 16:01:32 +0100

    refactor error validation

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,11 +4,15 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @error_messages = {
+      valid_amount: "Please provide a valid amount",
+      exceed_limit: "Sorry, you cannot exceed the balance limit of £#{BALANCE_LIMIT}",
+    }
   end
 
   def top_up(amount)
-    fail "Please provide a valid amount" unless valid_amount?(amount)
-    fail "Sorry, you cannot exceed the balance limit of £#{BALANCE_LIMIT}" if @balance + amount > BALANCE_LIMIT
+    fail @error_messages[:valid_amount] unless valid_amount?(amount)
+    fail @error_messages[:exceed_limit] if exceed_limit?(amount)
     @balance += amount
   end
 
@@ -16,5 +20,9 @@ class Oystercard
 
   def valid_amount?(amount)
     (amount.is_a?(Integer) || amount.is_a?(Float)) && (amount.positive?)
+  end
+
+  def exceed_limit?(amount)
+    @balance + amount > BALANCE_LIMIT
   end
 end
