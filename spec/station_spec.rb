d58aa1a49d93ff64085efe0c0e307915d719bfb2
parsa <parsamesg@gmail.com>
2021-09-28 15:51:59 +0100

    create unit test for station class to access name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,4 +1,12 @@
-require 'station'
+require "station"
+
 describe Station do
-  
+  it "creates station with name" do
+    station = Station.new("Kings Cross", 1)
+    expect(station.name).to eq "Kings Cross"
+  end
+  it "creates station with zone" do
+    station = Station.new("Kings Cross", 1)
+    expect(station.zone).to eq 1
+  end
 end
